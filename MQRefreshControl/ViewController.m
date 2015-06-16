0c6fd013e366355492e1e28e322c9f7ff90a6383
李铁柱 <1262111739@qq.com>
2015-06-16 17:53:57 +0800

    一个支持上下左右刷新的控件

--- MQRefreshControl/ViewController.m
+++ MQRefreshControl/ViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "UIScrollView+MQRefreshControl.h"
 
 @interface ViewController ()
-
+{
+    UIScrollView * scrollView;
+}
 @end
 
 @implementation ViewController
@@ -17,6 +20,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:scrollView];
+    
+    
+    
+    // 添加刷新方向
+    [scrollView addRefreshControl:RefreshDirection_Top];
+    [scrollView addRefreshControl:RefreshDirection_Bottom];
+    [scrollView addRefreshControl:RefreshDirection_Left];
+    [scrollView addRefreshControl:RefreshDirection_Right];
+    
+    [scrollView setRefreshBlock:^ (RefreshDirection direction){
+
+//        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//            [weakSelf->scrollView endRefreshDirection:direction];
+//        });
+    }];
+//    [scrollView beganRefreshDirection:RefreshDirection_Bottom];
+    scrollView.contentSize = CGSizeMake(scrollView.frame.size.width, 1000);
+    scrollView.backgroundColor = [UIColor clearColor];
 }
 
 - (void)didReceiveMemoryWarning {
